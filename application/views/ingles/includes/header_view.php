bc7dad59d4a79d9636ffc39cbb136464178fc635
Alejandro Sanchez <alejandro.sanchez@regxi.com>
2021-05-17 11:03:46 -0300

    Pixel

--- application/views/ingles/includes/header_view.php
+++ application/views/ingles/includes/header_view.php
@@ -38,6 +38,8 @@
 	&noscript=1"/>
 	</noscript>
 	<!-- End Facebook Pixel Code -->
+
+	<script>function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"9d45829279d13443ee36c6709396eb7e"})});</script>
 </head>
 
 
